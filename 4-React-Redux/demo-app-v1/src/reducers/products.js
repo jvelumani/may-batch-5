030e14cc4ea6b16a1fe05830fe7654d112af51af
Nagabhushanam <nagabhushanamn@gmail.com>
2018-05-30 11:31:36 +0530

    bug fix

--- 4-React-Redux/demo-app-v1/src/reducers/products.js
+++ 4-React-Redux/demo-app-v1/src/reducers/products.js
@@ -3,7 +3,7 @@
 export function productsReducer(state = [], action) {
     switch (action.type) {
         case 'LOAD_PRODUCTS':
-            return [...state, ...action.products];
+            return [...action.products];
         default:
             return state;
     }
